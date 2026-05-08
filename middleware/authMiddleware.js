c5f92e9f27141d56c71c32ef4052e98e254d92ad
ChoupiPlanete <1064152@bdeb.qc.ca>
2026-05-07 23:24:23 -0400

    Ajout de commentaire pour ma compréhension + division du html

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,22 @@
-const jwt = require('jsonwebtoken');
+//-----gestion de connexion côté serveur
+const jwt = require('jsonwebtoken');      //----librairie qui crée et vérifie les tokens
 
 module.exports = (req, res, next) => {
     const header = req.headers.authorization;
 
-    if (!header || !header.startsWith('Bearer ')) {
+    if (!header || !header.startsWith('Bearer ')) {      //-----verifie si le token existe
         return res.status(401).json({ message: 'Token manquant ou invalide' });
     }
 
-    const token = header.split(' ')[1];
+    const token = header.split(' ')[1];     //-----extrait le token
 
     try {
-        const decoded = jwt.verify(token, 'secretkey');
+        const decoded = jwt.verify(token, 'secretkey');    //-----verifie la validité
         req.user = decoded;
         next();
     } catch (err) {
-        return res.status(403).json({ message: 'Token invalide ou expiré' });
+        return res.status(403).json({ message: 'Token invalide ou expiré' });    //----- affiche un message de refus
     }
 };
+
+//----- Sert à la connexion des utilisateurs
--- public/js/auth.js
+++ public/js/auth.js
@@ -1,24 +1,25 @@
-function saveToken(token) {
+//-----gestion de connexion côté front-end (navigateur)
+function saveToken(token) {     //----fonction qui sauvegarde le token
     localStorage.setItem('token', token);
 }
 
-function getToken() {
+function getToken() {     //----récupère le token sauvegardé
     return localStorage.getItem('token');
 }
 
-function logout() {
+function logout() {      //-----supprimme le token lorsque l'utilisateur se déconnecte
     localStorage.removeItem('token');
-    window.location.href = '/login.html';
+    window.location.href = '/login.html';   //-----redirection vers la page de login
 }
 
-function requireAuth() {
+function requireAuth() {     //----- vérifie si le token existe (pas s'il est valide, ça c'est le authMiddleware qui le fait)
     const token = getToken();
     if (!token) {
         window.location.href = '/login.html';
     }
 }
 
-async function apiFetch(url, options = {}) {
+async function apiFetch(url, options = {}) {     //-----asynch = fonction qui peut attendre des opérations lentes, mais qui permet au reste du site de fonctionner en attendant
     const token = getToken();
 
     const headers = {
@@ -35,7 +36,7 @@ async function apiFetch(url, options = {}) {
         headers
     });
 
-    if (response.status === 401 || response.status === 403) {
+    if (response.status === 401 || response.status === 403) {   //-----401> pas connecté    403> token invalide
         logout();
         throw new Error('Session expirée. Veuillez vous reconnecter.');
     }
