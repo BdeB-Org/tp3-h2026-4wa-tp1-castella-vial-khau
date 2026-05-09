746ba372194e67dec01f28aa30d110d071856f45
ChoupiPlanete <1064152@bdeb.qc.ca>
2026-05-09 14:19:24 -0400

    ajout route info animal dans le server

--- server.js
+++ server.js
@@ -8,14 +8,26 @@ require('./Config/database');
 app.use(express.json());
 app.use(express.static('public'));
 
+//----- routes auth
 const authRoutes = require('./Routes/authRoutes');
-
 app.use('/api/auth', authRoutes);
+
+//-----connexion route informations de l'animal ---- Sandra
+const animalRoutes = require("./Routes/informations_de_lanimal_Route");
+
+app.use("/", animalRoutes);
+
+app.get("/", (req,res)=> {
+    res.send("Bienvenu sur l'API");
+})
+//----------------------------------------------
+
 
 // Redirection par défaut
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
