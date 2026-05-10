3e8d19baff1f44a0055b90ff351d9539a3659ad1
ChoupiPlanete <1064152@bdeb.qc.ca>
2026-05-10 09:38:10 -0400

    modification info animal route

--- Routes/informations_de_lanimal_Route.js
+++ Routes/informations_de_lanimal_Route.js
@@ -1,21 +1,22 @@
-//Fait par Brandon Khau TP2   --------
+//Fait par Brandon Khau TP2   --------  Sandra pour le TP3
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
 
 const animalControllers = require("../Controllers/informations_de_lanimal_Controllers");
 
 //Route Get
-router.get("/Animal", animalControllers.getAnimals);   //--- tous les animaux
+router.get("/Animal",authMiddleware,  animalControllers.getAnimals)   //--- tous les animaux
 
-router.get("/Animal/:id", animalControllers.getAnimalById);  //--- 1 animal
+router.get("/Animal/:id", authMiddleware,  animalControllers.getAnimalById);  //--- 1 animal
 
 //Route Post
-router.post("/Animal", animalControllers.addAnimal);
+router.post("/Animal", authMiddleware,  animalControllers.addAnimal);
 
 //Route Update
-router.put("/Animal/:id", animalControllers.updateAnimal);
+router.put("/Animal/:id", authMiddleware, animalControllers.updateAnimal);
 
 //Route Delete
-router.delete("/Animal/:id", animalControllers.deleteAnimal);
+router.delete("/Animal/:id", authMiddleware,  animalControllers.deleteAnimal);
 
 module.exports = router;
